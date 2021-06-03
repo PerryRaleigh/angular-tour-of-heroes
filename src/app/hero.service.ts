8fbc1ebbca344b4f0bbd723a14fa4df3f20e750a
perryraleigh <perry.raleigh@gmail.com>
2021-06-03 07:58:09 -0600

    Updates to hero.service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,7 +4,6 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 
 @Injectable({ providedIn: 'root' })
